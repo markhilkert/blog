ad1d3757ce515c285b79d7baaf47c7cfbf708d24
markhilkert <mark.hilkert@gmail.com>
2019-04-14 09:38:39 -0700

    added formatted tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,15 @@ class Post < ApplicationRecord
   has_many :posts_tags
   has_many :tags, through: :posts_tags
   has_many :comments
+
+  def formatted_tags
+    formatted_tags = []
+
+    (self.tags.length - 1).times do |index|
+      formatted_tags << self.tags[index].name + ","
+    end
+    formatted_tags << self.tags[-1].name
+
+    formatted_tags
+  end
 end
