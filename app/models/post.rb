b16c99b364e2781cb1857991320362573cd1d280
markhilkert <mark.hilkert@gmail.com>
2019-04-15 08:46:29 -0700

    going to kill this branch, it made everything worse

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,7 @@ class Post < ApplicationRecord
   has_many :tags, through: :posts_tags
   has_many :comments
 
-  def formatted_tags
-    formatted_tags = []
-
-    (self.tags.length - 1).times do |index|
-      formatted_tags << self.tags[index].name + ","
-    end
-    formatted_tags << self.tags[-1].name
-
-    formatted_tags
+  def tag_names
+    self.tags.map { |tag| tag.name }
   end
 end
--- app/views/api/posts/_post.json.jbuilder
+++ app/views/api/posts/_post.json.jbuilder
@@ -4,12 +4,7 @@ json.preview_image post.preview_image
 json.title post.title
 json.body post.body
 json.preview_text post.preview_text
-
-json.formatted do
-  json.tags do
-    json.array! post.formatted_tags
-  end
-end
+json.tag_names post.tag_names
 
 json.comments do
   json.array! post.comments, partial: 'api/comments/comment', as: :comment
